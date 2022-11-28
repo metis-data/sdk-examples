4939f342f4e7d5d67c024211ea0a9d6edd36adb5
Lior Zamir <liorz@metisdata.io>
2022-11-28 16:57:59 +0200

    itzik suggested changes
    update to 0.1.3

---
+++ js/node-express-postgress-sequelize/models/init.js
@@ -0,0 +1,37 @@
+const process = require("process");
+const Sequelize = require("sequelize");
+const {creds} = require('../config');
+let db;
+
+function getInstance() {
+    if (!db) {
+        db = {};
+        const Sequelize = require("sequelize");
+        db.sequelize = new Sequelize(
+            creds.database,
+            creds.username,
+            creds.password,
+            creds,
+        );
+        db.Sequelize = Sequelize;
+    }
+
+    return db;
+}
+
+
+function initSequelize() {
+    const db = getInstance();
+    Object.keys(db).forEach((modelName) => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
+    });
+
+    return db;
+}
+
+module.exports = {
+    initSequelize,
+    getInstance,
+}
