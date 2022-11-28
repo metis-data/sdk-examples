50613811b7bb8ebce01b971f8564cd730e01795d
Lior Zamir <liorz@metisdata.io>
2022-11-28 16:57:59 +0200

    update to 0.1.3

--- js/node-express-postgress-sequelize/models/index.js
+++ js/node-express-postgress-sequelize/models/index.js
@@ -7,7 +7,6 @@ const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const SequelizeExpressInterceptor = require('@metis-data/sequelize-express-interceptor').default;
 const db = {};
 
 let sequelize;
@@ -31,31 +30,6 @@ Object.keys(db).forEach((modelName) => {
     db[modelName].associate(db);
   }
 });
-
-/**
- *  START METIS REQUIRED MIDDLEWARE
- */
-
-const interceptor = SequelizeExpressInterceptor.create({
-  serviceName: 'your-service-name', // The name of the service
-  serviceVersion: '0.0.1', // The version of the service
-  exporterUrl: 'https://ingest-stg.metisdata.io',
-  exporterApiKey: 'METIS_API_KEY', // The api key from https://app.metisdata.io/
-});
-
-interceptor.instrument(
-  sequelize, // The Sequelize instance for getting the plan
-  {
-    errorHandler: console.error, // Error handler, errors are still reporterd to metis' Sentry account
-    shouldCollectPlans: true, // Get the plan for each intercepted query (default to true)
-    excludedUrls: [/favicon.ico/], // URLs to exclude from tracing
-    printToConsole: true, // Print outgoing spans in console (default to false, passed to exporter)
-  }
-);
-
-/**
- *  END METIS REQUIRED MIDDLEWARE
- */
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
