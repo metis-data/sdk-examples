9dde8ace4a3e14e9ebf0c6cfc8e35722a54fef8d
Lior Zamir <liorz@metisdata.io>
2022-12-08 11:55:30 +0200

    itzik suggested changes

--- js/node-express-postgress-sequelize/models/init.js
+++ js/node-express-postgress-sequelize/models/init.js
@@ -1,27 +1,21 @@
-const process = require("process");
+const { creds } = require('../config');
 const Sequelize = require("sequelize");
-const {creds} = require('../config');
-let db;
+let db = {};
 
 function getInstance() {
-    if (!db) {
-        db = {};
-        const Sequelize = require("sequelize");
-        db.sequelize = new Sequelize(
-            creds.database,
-            creds.username,
-            creds.password,
-            creds,
-        );
-        db.Sequelize = Sequelize;
-    }
-
     return db;
 }
 
-
 function initSequelize() {
-    const db = getInstance();
+    const Sequelize = require("sequelize");
+    db.sequelize = new Sequelize(
+        creds.database,
+        creds.username,
+        creds.password,
+        creds,
+    );
+    db.Sequelize = Sequelize;
+
     Object.keys(db).forEach((modelName) => {
         if (db[modelName].associate) {
             db[modelName].associate(db);
