76c81a377ff6d4eec615826e1f916253c7014fa7
Lior Zamir <liorz@metisdata.io>
2022-12-27 18:04:38 +0200

    update sequlize

--- js/node-express-postgress-sequelize/config/config.js
+++ js/node-express-postgress-sequelize/config/config.js
@@ -1,24 +1,27 @@
 const creds = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: '127.0.0.1',
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
     dialect: 'postgres',
+    port: process.env.PGPORT
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
     dialect: 'postgres',
+    port: process.env.PGPORT
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
     dialect: 'postgres',
+    port: process.env.PGPORT
   },
 };
 
