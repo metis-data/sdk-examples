96b7f2c6126bcc35f429c717edb2b1b2b70a3d88
Issackr <issackr@metisdata.io>
2023-03-07 16:53:17 +0200

    archived sequelize example and updated prisma example to work with prisma-interceptor

--- js/node-nestjs-postgrs-prisma/src/main.ts
+++ js/node-nestjs-postgrs-prisma/src/main.ts
@@ -1,20 +1,12 @@
 // src/main.ts
 
+import { startMetisInstrumentation } from './tracer';
+startMetisInstrumentation();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { PrismaNestInterceptor } from '@metis-data/prisma-nest-interceptor';
-
-const { METIS_SERVICE_NAME: serviceName, METIS_SERVICE_VERSION: serviceVersion, METIS_API_KEY: apiKey } = process.env;
-
-const interceptor = PrismaNestInterceptor.create({
-  serviceName,
-  serviceVersion,
-  apiKey
-});
-
-interceptor.instrument({ excludedUrls: [/favicon.ico/] });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
---
+++ js/node-nestjs-postgrs-prisma/src/tracer.ts
@@ -0,0 +1,39 @@
+import opentelemetry from '@opentelemetry/api';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { Resource } from '@opentelemetry/resources';
+import {
+  BasicTracerProvider,
+  BatchSpanProcessor,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { AsyncHooksContextManager } from '@opentelemetry/context-async-hooks';
+import { getPrismaInstrumentation, getMetisExporter, MetisHttpInstrumentation } from '@metis-data/prisma-interceptor';
+
+export const startMetisInstrumentation = () => {
+  const tracerProvider = new BasicTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: process.env.METIS_SERVICE_NAME,
+      [SemanticResourceAttributes.SERVICE_VERSION]: process.env.METIS_SERVICE_VERSION,
+    }),
+  });
+
+  const metisExporter = getMetisExporter(process.env.METIS_API_KEY);
+
+  tracerProvider.addSpanProcessor(new BatchSpanProcessor(metisExporter));
+
+  if (process.env.OTEL_DEBUG) {
+    tracerProvider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+  }
+
+  const contextManager = new AsyncHooksContextManager().enable();
+  opentelemetry.context.setGlobalContextManager(contextManager);
+
+  tracerProvider.register();
+
+  const excludeUrls = [/favicon.ico/];
+  registerInstrumentations({
+    instrumentations: [new MetisHttpInstrumentation(excludeUrls), getPrismaInstrumentation()],
+  });
+};
